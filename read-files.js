e3edfafb2a743a15421d810c8dac1b5385642f46
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-08-28 08:56:07 -0500

    limits to ten

--- read-files.js
+++ read-files.js
@@ -1,11 +1,10 @@
 const { createReadStream } = require('fs');
 const { Writable } = require('stream');
 const { map, split } = require('event-stream');
-const limitToTen = require('./limit-ten.js');
+const limitToTen = require('./limit-ten.js')();
 
 const userInput = process.argv[2] ? process.argv[2].toLowerCase() : null;
 const writeStream = Writable();
-//const worldListStream = createReadStream("/User/EmLem/AppData/Roaming/Microsoft/Spelling/en-US/default.dic");
 const worldListStream = createReadStream("./en-US/en-US/en-US.dic");
 
 writeStream._write = (word, _, next) => {
@@ -26,7 +25,7 @@ worldListStream
     word.toString().toLowerCase().includes(userInput) ? done(null, word+"\n"): done();
     })
 )
-//.pipe(limitToTen)
+.pipe(limitToTen)
 .pipe(writeStream)
 
 worldListStream.on('end', function() {
